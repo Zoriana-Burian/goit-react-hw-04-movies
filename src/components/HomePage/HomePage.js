84595ed6c7577bf19ea39b9c9236265f04a121c0
Zoriana-Burian <64468262+Zoriana-Burian@users.noreply.github.com>
2021-02-24 14:38:04 +0200

    виправлено помилку в імпорті

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,26 +1,32 @@
 import { useState, useEffect } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
-import fetchAPI from '../../servises/api-servises';
+import * as fetchAPI from '../../servises/api-servises';
 
 
 export default function HomePage() {
   const { url } = useRouteMatch();
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-        fetchAPI.fetchMovies()
-            .then(setMovies);
-    }, [])
+useEffect(() => {
+    fetchAPI.fetchMovies().then(moviesTrend => {
+      setMovies(moviesTrend.results);
+    });
+  }, []);
 
   return (
-    <div>
+    <>
+      
+      {movies && (
         <ul>
           {movies.map(movie => (
-            <li key={movie.id}><Link to={`${url}/${movie.id}`}>{movie.title}</Link>
+            <li key={movie.id}>
+              <Link to={`${url}/${movie.id}`}>{movie.title}</Link>
             </li>
           ))}
         </ul>
-    </div>
+      )}
+    </>
   );
 }
+
 
--- src/servises/api-servises.js
+++ src/servises/api-servises.js
@@ -3,7 +3,7 @@
 //eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjQ0YTI4MTM3ZDRkMjJjZDUyYTg0MTc4MGNiMmQ1NCIsInN1YiI6IjYwMmNlMjdiY2VkYWM0MDAzZTUwZWE5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QSTFMjfZZkmtPFytIWyAwu7vTaM4K7xySsoTOJV8Q-Y
   
   
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 const KEY = '7644a28137d4d22cd52a841780cb2d54';
 
@@ -14,9 +14,7 @@ async function fetchWithErrorHandling(url = '', config = {}) {
     : Promise.reject(new Error('Not found'));
 }
 
- function fetchMovies() {
-  return fetchWithErrorHandling(`${BASE_URL}trending/movie/day?api_key=${KEY}`);
+ export function fetchMovies() {
+  return fetchWithErrorHandling(`${BASE_URL}/trending/movie/day?api_key=${KEY}`);
 }
 
-const fetchApi = { fetchMovies };
-export default  fetchApi;
